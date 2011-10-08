ea1d1d40e8e451f609b9812e63f71cea2f2d7ebf
Charles Butler <coder.chuck@gmail.com>
2011-10-08 14:31:13 -0400

    Adding the git ignore

---
+++ OctoBot/ConfigFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctoBot
+{
+    class ConfigFile
+    {
+
+        // Static method to open the config file and read values
+
+
+    }
+}
